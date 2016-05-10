3162ed681a1cac18fbe20763fd738cfdd2711886
Asier <Asier@Asier>
2016-05-10 11:28:48 +0200

    Implementado para testear middleware

--- src/demo/Receptor.java
+++ src/demo/Receptor.java
@@ -10,11 +10,17 @@ public class Receptor {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		PsPortFactory conexion = new PsPortFactory();
 		PsPort port = conexion.getPort("direccionPrueba");
+		
 		port.start();
+		
 		while(true){
-		Thread.sleep(10000);
-		String dato = port.getLastSample(0, null, 4);
-		System.out.println("Receptor recibe: "+dato);
+			Thread.sleep(6000);
+			String dato = port.getLastSample(0, null, 4);
+			System.out.println("Receptor recibe: "+dato);
+			String dato2 = port.getLastSample(0, null, 4);
+			System.out.println("Receptor recibe: "+dato2);
+			String dato3 = port.getLastSample(1, null, 4);
+			System.out.println("Receptor2 recibe: "+dato3);
 		}
 	}
 
