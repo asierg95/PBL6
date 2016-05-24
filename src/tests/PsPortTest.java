4c0d4d2d30631ca4ada9f2cba6b6a19d4e847b5c
Asier <Asier@Asier>
2016-05-24 16:21:56 +0200

    test before added

--- src/tests/PsPortTest.java
+++ src/tests/PsPortTest.java
@@ -1,17 +1,25 @@
 package tests;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import middleware.PsPort;
 
 public class PsPortTest {
+	PsPort port;
+	
+	@Before
+	public void setUp(){
+		port = new PsPort("middleware.conf");
+	}
 	
 	@Test
 	  public void testAssertArrayEquals() {
-		PsPort port = new PsPort("middleware.conf");
 	    byte[] expected = "1=kaixo".getBytes();
 	    byte[] actual = port.crearMensaje(1, "kaixo".getBytes());
 	    org.junit.Assert.assertArrayEquals("failure - byte arrays not same", expected, actual);
 	  }
+	
+	
 
 }
