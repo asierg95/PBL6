2d88773c7ba227ba7d05e9865e08d35cdd479519
Xabier <xabigau@gmail.com>
2016-05-30 17:22:06 +0200

    Emisor and Receptor created again

---
+++ src/demo/Emisor.java
@@ -0,0 +1,34 @@
+package demo;
+
+import java.util.Scanner;
+
+import middleware.Publisher;
+
+public class Emisor {
+
+	public static void main(String[] args) {
+		String mensaje;
+		Publisher publicador = new Publisher();
+		boolean exit = false;
+		Scanner teclado = new Scanner(System.in);
+		int id, len, respuesta;
+		String fichConf = "middleware.conf";
+		publicador.iniciarConexion(fichConf);
+
+		
+		System.out.println("Ya se pueden mandar mensajes");
+		while(!exit){
+			System.out.println("Mensaje: ");
+			mensaje = teclado.next();
+			System.out.println("Cual es el id del dato?");
+			id = Integer.valueOf(teclado.next());
+			System.out.println("Cual es la longitud del dato?");
+			len = Integer.valueOf(teclado.next());
+			respuesta = publicador.send(id, mensaje, len);
+			System.out.println(respuesta);
+		}
+		
+	}
+
+}
+
